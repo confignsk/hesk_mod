59250c8aa834be585183cd0613d0ea002725a3c0
confignsk <confignsk@ya.ru>
2022-02-22 11:19:32 +0700

    Start

---
+++ admin.menu.php
@@ -0,0 +1,43 @@
+<?
+$aMenuLinks = array(
+    array(
+        "Клиентская зона",
+        "/",
+        array(),
+        array(),
+        ""
+    ),
+
+
+    array(
+        "Сайт Телематика",
+        "https://tlm.su",
+        array(),
+        array(),
+        ""
+    ),
+
+    array(
+        "Сайт Ваш Сервис",
+        "https://2870700.ru",
+        array(),
+        array(),
+        ""
+    ),
+
+    array(
+        "CRM Ваш Сервис",
+        "https://2870700.scsoft.ru",
+        array(),
+        array(),
+        ""
+    ),
+
+    array(
+        "CRM Телематика",
+        "https://configdev.bitrix24.ru",
+        array(),
+        array(),
+        ""
+    ),
+);
