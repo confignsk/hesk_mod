0c66362150f84ade9ed61502bea047961eb0e56c
confignsk <confignsk@ya.ru>
2022-02-22 11:15:08 +0700

    Start

--- inc/tlm_mod/mail.php
+++
@@ -1,6 +0,0 @@
-<?
-$headers = 'From: m@tlm.su' . "\r\n" .
-    'Reply-To: m@tlm.su' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-echo (mail("pipe@help.tlm.su", "Subject", "Message", $headers)) ? "send" : "fail";
